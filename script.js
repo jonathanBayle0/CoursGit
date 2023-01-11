57ce53b7a55e44ff24ecfc98b912cd8762dbe85e
Jonathan Bayle <jonathan.bayle002@gmail.com>
2023-01-11 08:30:31 +0100

    meilleur nommage de variable

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const a = 12;
-let b = 45;
+const operande = 12;
+let operande2 = 45;
 
-b += a ;
-console.log("resultat de a + b = " + b);
+let result = operande2 + operande ;
+console.log("resultat de " + operande + " + " + operande2 + " = " + result);

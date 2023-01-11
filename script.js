68de348f2c66b54158ad410d5b1de2d21e484187
jonathanBayle0 <jonathan.bayle002@gmail.com>
2023-01-11 09:08:21 +0100

    Rebase interactif de la branche master

--- script.js
+++ script.js
@@ -3,3 +3,38 @@ let operande2 = 45;
 
 let result = operande2 + operande ;
 console.log("resultat de " + operande + " + " + operande2 + " = " + result);
+
+let aPerson = { 
+	name: 'Jean',
+	age: 35,
+	gender: 'masculin',
+	interest: ['musique', 'Jeux videos'],
+};
+
+aPerson.sayHello = function() {console.log('Bonjour ! Je suis ' + this.name + '.'); };
+
+aPerson.sayHello();
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Dimanche";
+    break;
+  case 1:
+    day = "Lundi";
+    break;
+  case 2:
+     day = "Mardi";
+    break;
+  case 3:
+    day = "Mercredi";
+    break;
+  case 4:
+    day = "Jeudi";
+    break;
+  case 5:
+    day = "Vendredi";
+    break;
+  case 6:
+    day = "Samedi";
+}
+console.log("Aujourd'hui nous sommes " + day);

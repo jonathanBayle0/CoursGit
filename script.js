b0db85cf3967bcf69bde9b22ca47d6c0bd50927a
MarieLeBuhan <marie.lebuhan@gmail.com>
2023-01-24 11:59:38 +0100

    feat: init() en cours

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function generateur() {
   const nb = Math.floor(Math.random() * 100);
   return nb;
 }
+
+var nbAleatoire;
+function init() {
+  nbAleatoire = generateur();
+  /*var saisie="";
+  while saisie<> {
+    saisie =document.getElementById("guess").value;
+  }*/
+}
